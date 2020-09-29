f662f013aa2ee10d6b25bacc9d0209e59b13bd61
pboox <cajaspedro@hotmail.com>
2020-09-28 18:52:18 -0600

    configuracion lista del curso practico reactjs

---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../assets/styles/App.scss'
+
+const HelloWorld = () => (
+  <h1>Hola Mundo</h1>
+  // return (
+  //   <div>
+      
+  //   </div>
+  // )
+);
+
+export default HelloWorld;
+
